1e7f40d1f54764fc6e098553ac05eed5c9a0c718
aarya4774 <aaryapdiwan@gmail.com>
2025-04-12 18:10:53 +0530

    initial commit

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// Firebase 8.x compatible config file
+var firebaseConfig = {
+  apiKey: "AIzaSyDWfm_Hz7tt3zCyQ0LdFA7xT-3UvqKF6M4",
+  authDomain: "ambulance-8d429.firebaseapp.com",
+  databaseURL: "https://ambulance-8d429-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ambulance-8d429",
+  storageBucket: "ambulance-8d429.firebasestorage.app",
+  messagingSenderId: "54313817912",
+  appId: "1:54313817912:web:6041adeb38c20e588453cd",
+  measurementId: "G-FV5PGBF23W"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
